cbe7ab37cd11adfe6f5aa6201ac0f44136f621c2
yakelixir <yakelixir@gmail.com>
2019-06-04 14:54:22 -0500

    test changes, Jenkinsfie updates, Readme to reflect such

--- README.md
+++ README.md
@@ -4,3 +4,19 @@ run a test of the local host
 
 MACOSX Help at:
 https://wiki.jenkins.io/display/JENKINS/Thanks+for+using+OSX+Installer
+
+DEV NOTES
+
+Additons to follow:
+
+* Code commit to main or release branch: when tests pass, push an artifact, such as pip package or docker image, or an artifact repository
+* Submission of pull/merge request: run tests and provide feedback to git server, such as GitHub or GitLab, and block submission approval if tests fail.
+
+For visual feed back to the a git server:
+
+* webhooks on those services
+* token generated with authorization to access the repository (or an account with access)
+* Jenkins plug-in (GitLab plugin and GitHub plugin)
+
+PROJECT PRIMER
+https://medium.com/@Joachim8675309/jenkins-ci-pipeline-with-python-8bf1a0234ec3 # Thanks!
--- test_app.py
+++ test_app.py
@@ -7,25 +7,51 @@ from jenkins import app
 class TestHello(unittest.TestCase):
 
     def setUp(self):
+        """
+        SETUP app
+        :return:
+        """
         app.app.testing = True
         self.app = app.app.test_client()
 
     def test_hello(self):
+        """
+        Check status code & data returned from get.(/)
+        : @ host @ route
+        : @ host data
+        :return:
+        """
         rv = self.app.get('/')
         self.assertEqual(rv.status, '200 OK')
         self.assertEqual(rv.data, b'Hello World!\n')
 
     def test_hello_hello(self):
+        """
+        Check status code & data returned from get.(/hello/)
+        : @ host @ route
+        : @ host data
+        :return:
+        """
         rv = self.app.get('/hello/')
         self.assertEqual(rv.status, '200 OK')
         self.assertEqual(rv.data, b'Hello World!\n')
 
     def test_hello_name(self):
+        """
+        Check status code & data returned from get.(/hello/simon)
+        : @ host @ route
+        : @ host data
+        :return:
+        """
         name = 'Simon'
         rv = self.app.get(f'/hello/{name}')
         self.assertEqual(rv.status, '200 OK')
         self.assertIn(bytearray(f"{name}", 'utf-8'), rv.data)
 
 if __name__ == '__main__':
+
+    import xmlrunner
+
+    runner = xmlrunner.XMLTestRunner(output='test-reports')
+    unittest.main(testRunner=runner)
     unittest.main()
-    # chmod +x test.py
